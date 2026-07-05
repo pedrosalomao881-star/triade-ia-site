1c09756d312f16d9f145c07a18a13ef459d0e882
archtx <archtx@localhost.localdomain>
2026-07-04 22:48:49 -0300

    fix: Configure Vercel for static file serving

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from next;
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  poweredByHeader: false,
+  staticPageGenerationTimeout: 300,
 };
 
 export default nextConfig;
